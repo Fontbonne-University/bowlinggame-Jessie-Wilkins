37d15f715758587a706d931184aba5aa3a0a7c77
wilkinsj <wilkinsj@R416-07-780.Fontbonne.edu>
2019-03-26 15:20:20 -0500

    Got to failure on strike

--- BowlingGame/Bowling/src/BowlingGame/BowlingTest.java
+++ BowlingGame/Bowling/src/BowlingGame/BowlingTest.java
@@ -8,14 +8,67 @@ import org.junit.Test;
 
 public class BowlingTest {
 
+	Game g;
+	
+	private void rollMany(int n, int pins) {
+		for(int i = 0; i<n;i++)
+			g.roll(pins);
+	}
+	
+	private void rollSpare() {
+		g.roll(5);
+		g.roll(5); //spare
+	}
+	
 	@Before
 	public void setUp() throws Exception {
-
+		g = new Game();
 	}
 
 	@Test
 	public void canCreateGame()
 	{
-		Game g = new Game();
+		g = new Game();
 	}
+	
+	@Test
+	public void canRoll() {
+		
+		g.roll(0);
+	}
+	
+	@Test
+	public void GutterGame() {
+		rollMany(20, 0);
+		assertEquals(0, g.score());
+	}
+	
+	@Test
+	public void allOnes()
+	{
+		rollMany(20, 1);
+		assertEquals(20, g.score());
+	}
+	
+	//@Ignore
+	@Test
+	public void oneSpare() {
+		rollSpare();
+		g.roll(3);
+		rollMany(17,0);
+		assertEquals(16, g.score());
+	}
+
+	@Test
+	public void oneStrike() {
+		g.roll(10);
+		g.roll(3);
+		g.roll(4);
+		rollMany(16, 0);
+		assertEquals(24, g.score());
+	
+	
+	}
+
+	
 }
--- BowlingGame/Bowling/src/BowlingGame/Game.java
+++ BowlingGame/Bowling/src/BowlingGame/Game.java
@@ -2,5 +2,30 @@ package BowlingGame;
 
 public class Game {
 
+	int score = 0;
+	private int rolls[] = new int[21];
+	private int currentRoll = 0;
+	
+	public void roll(int pins) {
+		rolls[currentRoll++] = pins;
+		
+	}
+	
+	public int score() {
+		int firstInFrame =0;
+		for(int frame = 0; frame<10; frame++) {
+			if(rolls[firstInFrame]+rolls[firstInFrame+1] == 10) {
+				score +=10 + rolls[firstInFrame+2];
+				firstInFrame+=2;
+			}
+			else {
+				score+=rolls[firstInFrame]+rolls[firstInFrame+1];
+				firstInFrame+=2;
+			}
+			
+			
+		}
+		return score;
+	}
 
 }
